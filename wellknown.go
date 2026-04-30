c2cc547f66ef0f5ac27b24a48c50ea236c30c286
Yura Solovjov <y.solovjov@vkteam.ru>
2026-04-30 15:39:08 +0300

    Expand README with detailed explanation of Protobuf Reflection, usage examples, and library internals; remove unused WKT parsing code.

--- wellknown.go
+++ wellknown.go
@@ -32,45 +32,6 @@ func isWKT(name protoreflect.FullName) bool {
 	return wktNames[name]
 }
 
-func parseWKT(fd protoreflect.FieldDescriptor, s string) (protoreflect.Value, error) {
-	name := fd.Message().FullName()
-
-	switch name {
-	case "google.protobuf.Empty":
-		return protoreflect.Value{}, fmt.Errorf("google.protobuf.Empty does not support default_value")
-	case "google.protobuf.Any":
-		return protoreflect.Value{}, fmt.Errorf("google.protobuf.Any does not support default_value")
-	}
-
-	// For most WKT, we can use protojson.
-	// We create an instance of the WKT message and unmarshal the string.
-	// If it's a wrapper, it's just the scalar value.
-	// If it's Duration, it's "15s".
-	// If it's Timestamp, it's RFC3339.
-	// If it's Struct, it's JSON object.
-	// If it's FieldMask, it's "a,b,c".
-
-	// Special case for FieldMask because it might not be a valid JSON string if it's just a,b,c
-	if name == "google.protobuf.FieldMask" {
-		// FieldMask in JSON is a string. So we should wrap it in quotes if it's not.
-		if !strings.HasPrefix(s, "\"") {
-			s = fmt.Sprintf("%q", s)
-		}
-	} else if isWrapper(name) || name == "google.protobuf.Duration" || name == "google.protobuf.Timestamp" {
-		// These expect a JSON-compliant scalar (string with quotes for Duration/Timestamp/String, or raw for bool/number)
-		// Try to see if it needs quotes
-		if needsQuotes(name, s) {
-			s = fmt.Sprintf("%q", s)
-		}
-	}
-
-	// We can't easily create a dynamic message from just a descriptor without a registry.
-	// But we can use the proto.Clone/New if we have a concrete type.
-	// Since these are WKT, they are linked into the binary.
-
-	return protoreflect.Value{}, fmt.Errorf("use setWKTDefault instead")
-}
-
 func setWKTDefault(m protoreflect.Message, fd protoreflect.FieldDescriptor, s string) error {
 	name := fd.Message().FullName()
 	if name == "google.protobuf.Empty" {
@@ -95,10 +56,6 @@ func setWKTDefault(m protoreflect.Message, fd protoreflect.FieldDescriptor, s st
 
 	m.Set(fd, protoreflect.ValueOfMessage(newWKT))
 	return nil
-}
-
-func isWrapper(name protoreflect.FullName) bool {
-	return strings.HasSuffix(string(name), "Value") && strings.HasPrefix(string(name), "google.protobuf.")
 }
 
 func needsQuotes(name protoreflect.FullName, s string) bool {
